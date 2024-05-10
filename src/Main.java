34c89db12e9222b8e96b8128e157c133af5c1768
Oleg <oleg_goncharov@inbox.ru>
2024-05-10 09:56:41 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static String[] calc(String input) {
+        String[] strEl = input.split(" ");
+        if (strEl.length != 3) {
+            throw new IllegalArgumentException("Должно быть 2 операнда и 1 оператор");
+        }
+
+        if (!(strEl[1].equals("+") || strEl[1].equals("-") || strEl[1].equals("*") || strEl[1].equals("/"))) {
+            throw new IllegalArgumentException("Некорректный оператор");
+        }
+        HashMap<String, Integer> values = new HashMap<>();
+        values.put("I", 1);
+        values.put("II", 2);
+        values.put("III", 3);
+        values.put("IV", 4);
+        values.put("V", 5);
+        values.put("VI", 6);
+        values.put("VII", 7);
+        values.put("VIII", 8);
+        values.put("IX", 9);
+        values.put("X", 10);
+//        if ((!values.containsKey(strEl[0]) || !values.containsKey(strEl[2])) || (values.get(strEl[0]) < 1 || values.get(strEl[0]) > 10) || (values.get(strEl[2]) < 1 || values.get(strEl[2]) > 10)) {
+//            throw new IllegalArgumentException("Неверный операнд");
+//        }
+        int result;
+        if (values.containsKey(strEl[0]) && values.containsKey(strEl[2])) {
+            int operand1 = values.get(strEl[0]);
+            int operand2 = values.get(strEl[2]);
+            result = switch (strEl[1]) {
+                case "+" -> operand1 + operand2;
+                case "-" -> operand1 - operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> operand1 / operand2;
+                default -> throw new IllegalArgumentException("Некорректный оператор");
+            };
+            return new String[]{romanice(result)};
+        } else {
+            int operand1 = Integer.parseInt(strEl[0]);
+            int operand2 = Integer.parseInt(strEl[2]);
+            result = switch (strEl[1]) {
+                case "+" -> operand1 + operand2;
+                case "-" -> operand1 - operand2;
+                case "*" -> operand1 * operand2;
+                case "/" -> operand1 / operand2;
+                default -> throw new IllegalArgumentException("Некорректный оператор");
+            };
+            System.out.println(result);
+            return new String[]{Integer.toString(result)};
+        }
+
+    }
+    private static String romanice(int num) {
+        String[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < romanNumerals.length; i++) {
+            while (num >= values[i]) {
+                roman.append(romanNumerals[i]);
+
+                num -= values[i];
+            }
+        }
+        return roman.toString();
+    }
+     public static void main(String[] args) {
+        System.out.println(Arrays.toString(calc("9 / 3")));
+      }
+    }
+
